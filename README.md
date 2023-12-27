0ccafcba4355f328ca5ce34e8666208f8d4561fc
Dipesh <dipesh.stha70@gmail.com>
2023-12-27 22:39:14 +0545

    Removed Resolution Center

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
-Marketplace web app, Only for the demotration purpose.
-
-# Marketplace Project
+# Marketplace Demo Project
 
 ## Overview
 
-This repository contains the source code for a marketplace project developed using React, React Router, Redux, Thunk, Hooks, Axios, Formik, and React Bootstrap.
+This repository serves as a demonstration of a marketplace project built using React, React Router, Redux, Thunk, Hooks, Axios, Formik, and React Bootstrap. The project is designed for code demonstration purposes and showcases the integration of various technologies commonly used in modern web development.
 
 ## Features
 
-- **React Components:** Utilize reusable and modular React components to create a dynamic user interface.
-- **React Router:** Implement client-side routing for a seamless single-page application experience.
-- **Redux and Thunk:** Manage state globally and handle asynchronous actions for improved data flow.
-- **Hooks:** Leverage React Hooks for functional components, providing a cleaner and more concise code structure.
-- **Axios:** Make HTTP requests to interact with backend APIs and fetch data dynamically.
-- **Formik:** Simplify form management and validation for a better user input experience.
-- **React Bootstrap:** Utilize Bootstrap components with React integration for a responsive and visually appealing design.
+- **React Components:** Utilizes modular and reusable React components for building a dynamic user interface.
+- **React Router:** Implements client-side routing for seamless navigation within the application.
+- **Redux and Thunk:** Manages state globally and handles asynchronous actions for improved data flow.
+- **Hooks:** Leverages React Hooks for functional components, promoting a cleaner and more concise code structure.
+- **Axios:** Performs HTTP requests to interact with backend APIs, demonstrating data fetching and manipulation.
+- **Formik:** Simplifies form management and validation, providing an enhanced user input experience.
+- **React Bootstrap:** Integrates Bootstrap components with React for a responsive and visually appealing design.
--- src/components/MainSearch/MainSearch.js
+++ src/components/MainSearch/MainSearch.js
@@ -1,84 +1,84 @@
-import React, {useState} from "react";
-import {Button, Form} from "react-bootstrap";
-import {BiSearch} from "react-icons/bi";
-import {useHistory} from "react-router-dom";
-import styles from "./MainSearch.module.css";
-import SearchResults from "../Search/SearchResults";
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { BiSearch } from 'react-icons/bi';
+import { useHistory } from 'react-router-dom';
+import styles from './MainSearch.module.css';
+import SearchResults from '../Search/SearchResults';
 
 function MainSearch() {
-	const history = useHistory();
-	const [searchWord, setSearchWord] = useState("");
-	const [searchDiv, setSearchDiv] = useState(false);
-	const searchLength = localStorage.getItem("search");
-	let mainSearch = true;
-	const handleSearch = e => {
-		setSearchWord(e.target.value);
-		setSearchDiv(true);
-		!searchLength && !e.target.value && setSearchDiv(false);
-	};
+  const history = useHistory();
+  const [searchWord, setSearchWord] = useState('');
+  const [searchDiv, setSearchDiv] = useState(false);
+  const searchLength = localStorage.getItem('search');
+  let mainSearch = true;
+  const handleSearch = (e) => {
+    setSearchWord(e.target.value);
+    setSearchDiv(true);
+    !searchLength && !e.target.value && setSearchDiv(false);
+  };
 
-	const handleKeyPress = e => {
-		e.preventDefault();
-		setSearchDiv(false);
-		history.push(`/search/${searchWord}`);
-		var existing = localStorage.getItem("search");
-		existing = existing ? existing.split(",") : [];
-		existing.push(searchWord);
-		localStorage.setItem("search", existing.toString());
-	};
-	return (
-		<>
-			<div className={styles.mainSearchWrapper}>
-				<div className={styles.mainSearchContainer}>
-					<h1>
-						Find the perfect <i>freelance</i> services for your business
-					</h1>
-					<Form onSubmit={handleKeyPress}>
-						<Form.Group
-							className={styles.formGroup}
-							style={{position: "relative", marginBottom: "1rem"}}
-						>
-							<Form.Control
-								size='lg'
-								type='text'
-								value={searchWord}
-								placeholder='Try "Building Mobile App"'
-								className={styles.searchInput}
-								onChange={handleSearch}
-								onClick={() => searchLength && setSearchDiv(true)}
-							/>
-							<SearchResults
-								searchWord={searchWord}
-								setSearchWord={setSearchWord}
-								searchDiv={searchDiv}
-								setSearchDiv={setSearchDiv}
-								mainSearch={mainSearch}
-							/>
-							<BiSearch className={styles.searchIcon} />
-							{searchWord && (
-								<i
-									className={`fas fa-times ${styles.cancelIcon}`}
-									onClick={() => setSearchWord("")}
-								></i>
-							)}
-							<Button className={styles.searchButton} type='submit'>
-								Search
-							</Button>
-						</Form.Group>
-					</Form>
-					<div className={styles.popularTags}>
-						<ul>
-							<li>Popular:</li>
-							<li>Web and Mobile Design</li>
-							<li>Wordpress</li>
-							<li>Logo Design</li>
-							<li>Drop Shipping</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  const handleKeyPress = (e) => {
+    e.preventDefault();
+    setSearchDiv(false);
+    history.push(`/search/${searchWord}`);
+    var existing = localStorage.getItem('search');
+    existing = existing ? existing.split(',') : [];
+    existing.push(searchWord);
+    localStorage.setItem('search', existing.toString());
+  };
+  return (
+    <>
+      <div className={styles.mainSearchWrapper}>
+        <div className={styles.mainSearchContainer}>
+          <h1>
+            Find the perfect <i>freelance</i> services for your business
+          </h1>
+          <Form onSubmit={handleKeyPress}>
+            <Form.Group
+              className={styles.formGroup}
+              style={{ position: 'relative', marginBottom: '1rem' }}
+            >
+              <Form.Control
+                size="lg"
+                type="text"
+                value={searchWord}
+                placeholder='Try "Building Mobile App"'
+                className={styles.searchInput}
+                onChange={handleSearch}
+                onClick={() => searchLength && setSearchDiv(true)}
+              />
+              <SearchResults
+                searchWord={searchWord}
+                setSearchWord={setSearchWord}
+                searchDiv={searchDiv}
+                setSearchDiv={setSearchDiv}
+                mainSearch={mainSearch}
+              />
+              <BiSearch className={styles.searchIcon} />
+              {searchWord && (
+                <i
+                  className={`fas fa-times ${styles.cancelIcon}`}
+                  onClick={() => setSearchWord('')}
+                ></i>
+              )}
+              <Button className={styles.searchButton} type="submit">
+                Search
+              </Button>
+            </Form.Group>
+          </Form>
+          <div className={styles.popularTags}>
+            <ul>
+              <li>Popular:</li>
+              <li>Web and Mobile Design</li>
+              <li>Wordpress</li>
+              <li>Logo Design</li>
+              <li>Drop Shipping</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MainSearch;
